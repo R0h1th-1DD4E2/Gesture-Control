c0d69e7c6fda56bd2dff44a53182b443f9b0ef32
R0h1th-1DD4E2 <rohith.010404@gmail.com>
2024-05-08 00:38:39 +0530

    Initial File

---
+++ WiFiClientBasic.ino
@@ -0,0 +1,103 @@
+/*
+    This sketch sends a string to a TCP server, and prints a one-line response.
+    You must run a TCP server in your local network.
+    For example, on Linux you can use this command: nc -v -l 3000
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#ifndef STASSID
+#define STASSID "swift"
+#define STAPSK "eyantra@123"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+const char* host = "192.168.198.223";
+const uint16_t port = 3000;
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+  Serial.begin(115200);
+
+  // We start by connecting to a WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(500);
+}
+
+String decodeByte(int8_t byte) {
+    String result = "";
+
+    for (int i = 7; i >= 0; i--) { // Loop from 7 to 0 to extract each bit from the byte
+        result += String((byte >> i) & 1); // Extract the i-th bit and append it to the result string
+    }
+    
+    return result;
+     // Output the result string
+}
+
+
+void loop() {
+  Serial.print("connecting to ");
+  Serial.print(host);
+  Serial.print(':');
+  Serial.println(port);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    Serial.println("wait 5 sec...");
+    delay(5000);
+    return;
+  }
+
+  // This will send the request to the server
+
+  while (true) {
+    // uint8_t byte = client.read();
+    // if (byte != 255) {
+    //   Serial.println(byte);
+    // }
+
+    client.println("");
+    int8_t byte = client.read();
+    if(byte != -1){
+      Serial.println(decodeByte(byte));
+    }
+  }
+
+  // void actuateOnSignal(uint16_t signal){
+
+  // }
+
+  // // read back one line from server
+  // Serial.println("receiving from remote server");
+  // String line = client.readStringUntil('\r');
+  // Serial.println(line);
+
+  // Serial.println("closing connection");
+  // client.stop();
+
+  // Serial.println("wait 5 sec...");
+  // delay(5000);
+}
