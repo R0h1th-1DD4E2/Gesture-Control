153c5e6823cf3eb70d845da56d9ead452ecbc83a
sateesh <sateeshabbigeri4@gmail.com>
2024-05-09 18:50:39 +0530

    Added proper controller

--- voiceController.py
+++ voiceController.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import pipeline
 import sounddevice as sd
+import re
 
 class VoiceModule:
     def __init__(self):
@@ -19,9 +20,41 @@ class VoiceModule:
     def get_transcription(self):
         return self.pipe(self.record_audio(), batch_size=8)
 
+
+    def bot_function_string(self):
+        input()
+        print('Recording the voice ')
+        transcription = self.get_transcription()['text'].lower()  # Convert transcription to lowercase for case-insensitivity
+        
+
+        if "forward" in transcription:
+            if "right" in transcription:
+                return "DFRT"  # Forward Right
+            elif "left" in transcription:
+                return "DFLT"  # Forward Left
+            else:
+                return "FWD"   # Forward
+        elif "backward" in transcription:
+            if "right" in transcription:
+                return "DWRT"  # Backward Right
+            elif "left" in transcription:
+                return "DWLT"  # Backward Left
+            else:
+                return "BWD"   # Backward
+        elif "left" in transcription:
+            return "LT"   # Left
+        elif "right" in transcription:
+            return "RT"   # Right
+        elif "stop" in transcription:
+            return "STP"  # Stop
+        elif "change" in transcription:
+            return f"PWM {0} {0} {0} {0}"
+        else:
+            # If no movement command is recognized, return None or raise an exception
+            return None  # Or raise ValueError("Unrecognized command")
+
+
 voice = VoiceModule()
 
 while True:
-    input()
-    print('Recording the voice')
-    print(voice.get_transcription()['text'])
+    print("Command : " ,voice.bot_function_string())
