a3fa98a098cca4407ee99485db5c2bb6cd41aee0
R0h1th-1DD4E2 <rohith.010404@gmail.com>
2024-05-10 15:50:29 +0530

    Many Tweaks - Made a Controller to run on web - Small Changes in the GUI

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from hand_control import HandTrackingController
 from keyboard_control import KeyboardController
+import popup_box
 
 # Import Custom Functions
 import find_ip
@@ -16,14 +17,32 @@ class ControlGUI:
         self.create_widgets()
 
     def create_widgets(self):
-        self.hand_control_button = tk.Button(self.root, text="Hand Gesture Control", command=self.start_hand_control)
-        self.hand_control_button.pack(pady=10)
+        # Frame for buttons
+        button_frame = tk.Frame(self.root)
+        button_frame.pack(pady=10)
+        self.root.geometry("350x150")
+        # Create a frame inside the root window
+        self.main_frame = tk.Frame(self.root)
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.keyboard_control_button = tk.Button(self.root, text="Keyboard Control", command=self.start_keyboard_control)
-        self.keyboard_control_button.pack(pady=5)
+        # Hand Gesture Control Button
+        hand_icon = tk.PhotoImage(file="Assets/gesturecontrol.png")
+        hand_icon = hand_icon.subsample(2)
+        self.hand_control_button = tk.Button(button_frame, image=hand_icon, command=self.start_hand_control, borderwidth=2, relief="solid")
+        self.hand_control_button.image = hand_icon
+        self.hand_control_button.pack(side=tk.LEFT, padx=5)
 
+        # Keyboard Control Button
+        keyboard_icon = tk.PhotoImage(file="Assets/keyboard.png")
+        keyboard_icon = keyboard_icon.subsample(2)
+        self.keyboard_control_button = tk.Button(button_frame, image=keyboard_icon, command=self.start_keyboard_control, borderwidth=2, relief="solid")
+        self.keyboard_control_button.image = keyboard_icon
+        self.keyboard_control_button.pack(side=tk.LEFT, padx=5)
+
+        # Quit Button
         self.quit_button = tk.Button(self.root, text="Quit", command=self.quit_program)
         self.quit_button.pack(pady=5)
+
 
     def start_hand_control(self):
         self.hand_tracking_controller.run(True)
@@ -40,6 +59,7 @@ class ControlGUI:
 
 if __name__ == "__main__":
     # ESP address
+    popup_box.popup()
     esp_ip = find_ip.find_device_ip("48:55:19:f6:57:34")
     app = ControlGUI(f"ws://{esp_ip}:8080/")
     app.root.mainloop()
